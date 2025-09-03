797c234db753ca3daeede5d0bc2cfac7701dd601
Nameeta <nameeta.m26@gmail.com>
2025-09-03 22:49:30 +0530

    DAG

--- trial.py
+++ trial.py
@@ -10,47 +10,83 @@ def create_graph(vertices, edges):
     return graph
     
 
-def bfs(graph, start, end):
+def dfs(graph, start):
     parent = {}
+    k=[]
     visited = set()
-    queue = [start]
+    stack = [start]
     
     visited.add(start)
-    while queue:
-        vertex = queue.pop(0)
+    while stack:
+        vertex = stack.pop()
         #print("before ",queue)
-        ##print(vertex, end=" ")
-        if vertex==end:
-            break
-        for neighbor in graph[vertex]:
+        print(vertex, end=" ")
+        ##if vertex==end:
+          #  break
+        K=graph[vertex][::-1]
+        for neighbor in K:
             #print("neighbor ", neighbor)
             if neighbor not in visited:
-                parent[neighbor]=vertex
+                ###parent[neighbor]=vertex
+                
                 visited.add(neighbor)
-                queue.append(neighbor)
+                stack.append(neighbor)
                 #print("after ",queue)
-    print(parent)
-    X=end
-    L=[]
-    while vertex!=start:
-        L+=[X,]
-        X=parent[X]
-        vertex=X
-        if X==start:
-            L+=[X,]
-            break
-    L.reverse()
-    print(L)
+def topological_sort_util(graph, vertex, visited, stack):
+    visited.add(vertex)
+    for neighbor in graph[vertex]:
+        j=neighbor[0]
+        #print("aha ",neighbor)
+        if j not in visited:
+            #print(neighbor)
+            topological_sort_util(graph, j, visited, stack)
+    stack.append(vertex)  
+    #print("stack ",stack)
+    #print("OUT")
+    #print()
 
+def topological_sort(graph):
+    stack = []
+    visited = set()
+    for vertex in graph:
+        if vertex not in visited:
+            #print("vertex",vertex)
+            topological_sort_util(graph, vertex, visited, stack)
+    k=stack[::-1]
+    return k
+def DAG(graph, stack):
+    for vertex in stack:
+        for neighbor in graph[vertex]:
+            j=neighbor[0]
+            if stack.index(j)>stack.index(vertex):
+                d=stack.index(vertex)
+            print(d,vertex,"->",j)
+                
+
+
+    
+    
 
 # Example usage
-vertices = ['A', 'B', 'C', 'D', 'E', 'F']
-edges = [('A', 'B'), ('A', 'C'), ('B', 'D'), ('B', 'E'), ('C', 'F'), ('E', 'F')]
+#vertices = ['A', 'B', 'C', 'D', 'E', 'F']
+#edges = [('A', 'B'), ('A', 'C'), ('B', 'D'), ('B', 'E'), ('C', 'F'), ('E', 'F')]
                
 
 
 
-graph = create_graph(vertices, edges)
+#graph = create_graph(vertices, edges)
 
-print("BFS from  A to F:")
-bfs(graph, 'A', 'F')
+#print("DFS from  A :")
+#dfs(graph, 'A')
+
+print()
+graph = {
+    'A': [('B', 3), ('C', 2)],
+    'B': [('D', 4)],
+    'C': [('D', 1), ('E', -1)],
+    'D': [('E', 5)],
+    'E': []
+}
+k=topological_sort(graph)
+print("Topological Sort: ", k)
+DAG(graph, k)
